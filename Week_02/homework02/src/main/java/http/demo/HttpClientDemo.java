92446fae75ceaed28e41a8725a337278e902317d
chenrongquan_bxbank <chenrongquan@aibank.com>
2020-10-28 19:31:13 +0800

    Week02|使用HttpClient 或OkHttp

---
+++ Week_02/homework02/src/main/java/http/demo/HttpClientDemo.java
@@ -0,0 +1,34 @@
+package http.demo;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * @description:
+ * @author: chenrq
+ * @date: 2020年10月28日 19时23分
+ */
+public class HttpClientDemo {
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet httpGet = new HttpGet("http://localhost:8801");
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            System.out.println("响应状态：" + response.getStatusLine());
+            if (entity != null) {
+                System.out.println("响应长度：" + entity.getContentLength());
+                System.out.println("响应内容：" + EntityUtils.toString(entity));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
